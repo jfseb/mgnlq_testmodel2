f53dda18fd6ef210e3b5f1cab665bd7ce8ddfe2a
jfseb <jfsebpublic@gmail.com>
2020-10-10 20:04:34 +0200

    jest2

--- js/constants.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const MONGO_DBURL = "mongodb://localhost/testdb2";
-export declare const MONGOOSE_RECORD_REPLAY_FOLDER: any;
-export declare const RAW_MODEL_PATH: any;
-export declare const MODEL_PATH: any;
-export declare const ENV_NAME_MONGO_RECORD_REPLAY = "MGNLQ_TESTMODEL2_REPLAY";
--- src/constants1.ts
+++ src/constants1.ts
@@ -7,6 +7,7 @@ export const MONGOOSE_RECORD_REPLAY_FOLDER = './test/data/mongoose_record_replay
 export const ENV_NAME_MONGO_RECORD_REPLAY = 'MGNLQ_TESTMODEL_REPLAY';
 
 // this determines where the cache is written and read from!
-export const RAW_MODEL_PATH = path.normalize(__dirname + '/../testmodel');
+// we also use a local path now
+export const RAW_MODEL_PATH = path.normalize('./testmodel');
 export const MODEL_PATH = RAW_MODEL_PATH; 
 // path.normalize(__dirname + '/../testmodel');
